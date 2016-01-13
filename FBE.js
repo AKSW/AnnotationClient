7d02e4608aeca8092ddee11cdddbd243e87ce016
Roy Meissner <roy-meissner@gmx.net>
2016-01-13 14:15:53 +0100

    Added commit data format conversion

--- FBE.js
+++ FBE.js
@@ -19,7 +19,7 @@ var FBE = {
   createFeedbackModal: function() {
     var modal = '<div id="feedbackModal" class="modal fade" tabindex="-1" role="dialog">' +
       '  <div class="modal-dialog modal-lg">' +
-        '    <div class="modal-content" ng-controller="ModalController">{{dummy}}' +
+      '    <div class="modal-content" ng-controller="ModalController">{{dummy}}' +
       '      <div class="modal-header">' +
       '        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
       '        <h4 class="modal-title">Modal Title</h4>' +
@@ -30,10 +30,10 @@ var FBE = {
       '        <form id="feedbackForm" class="form-inline">' +
       '         <p class="help-block">Please input your Credentials.</p>' +
       '         <div class="form-group">' +
-      '             <input type="email" class="form-control" placeholder="Your email" required>' +
+      '             <input id="feedbackFormAuthor" type="email" class="form-control" placeholder="Your E-Mail" >' +
       '         </div>' +
       '         <div class="form-group">' +
-      '             <input type="text" class="form-control" placeholder="Your Message" required>' +
+      '             <input id="feedbackFormMessage" type="text" class="form-control" placeholder="Your Message" >' +
       '         </div>' +
       '        </form>' +
       '      </div>' +
@@ -45,13 +45,13 @@ var FBE = {
       '  </div>' +
       '</div>';
     $("body").append(modal);
-      
+
     //Restart AngularJS
     angular.element(document).ready(function() {
       angular.bootstrap(document);
     });
-      
-    $("feedbackModalSave").submit(FBE.sendFeedback);
+
+    $("#feedbackForm").submit(FBE.sendFeedback);
     $("#feedbackModal").on('show.bs.modal', FBE.fillFeedbackModal);
   },
 
@@ -61,7 +61,7 @@ var FBE = {
     /*var url = document.URL;
     console.log(url);*/
     var resource = $(document).find("title").text();
-    FBE.getTriples(resource);
+    //FBE.getTriples(resource);
     modal.find('.modal-title').text('Feedback on Ressource ' + $(document).find("title").text());
     //TODO fill in rdf content
   },
@@ -79,10 +79,40 @@ var FBE = {
 
   sendFeedback: function(event) {
     event.preventDefault();
-    console.log("submitted");
+    var parser = N3.Parser({
+      format: 'application/trig'
+    });
+    parser.parse('@prefix ex: <http://example.org/feedback#>.\n' +
+      '@prefix eccrev: <https://vocab.eccenca.com/revision/>.\n' +
+      '@prefix prov: <http://www.w3.org/ns/prov#>.\n' +
+      '@prefix xsd: <http://www.w3.org/2001/XMLSchema#>.\n' +
+      'ex:patch-9999999 a eccrev:Commit;\n' +
+      'eccrev:commitAuthor ex:' + $("#feedbackFormAuthor").val() + ';\n' +
+      'eccrev:commitMessage "' + $("#feedbackFormMessage").val() + '";\n' +
+      'prov:atTime "' + new Date().toISOString() + '"^^xsd:dateTime;\n' + //Format: 2015-12-17T13:37:00+01:00
+      'eccrev:sha256 "9999999"^^xsd:base64Binary;\n' +
+      'eccrev:deltaDelete ex:delete-9999999;\n' +
+      'eccrev:deltaInsert ex:insert-9999999.',
+      function(error, triple, prefixes) {
+        if (triple)
+          console.log(triple.subject, triple.predicate, triple.object, '.');
+        else
+          console.log("# That's all, folks!", prefixes);
+      });
+    var writer = N3.Writer({
+      format: 'application/trig'
+    });
+    /*writer.addTriples(getDiff());
+    //TODO add infos
+    writer.end(function(error, result) {
+      console.log(result + " " + error);
+    });*/
   },
 
-  getDiff: function() {},
+  getDiff: function() {
+    //TODO Build var diffs like: [{s,p,o},{s,p,o},...]
+    return diffs;
+  },
 
 };
 
@@ -93,8 +123,9 @@ $(document).ready(function() {
 
 var FBE_A = angular.module("LDOW2016PF", []);
 FBE_A.run(function($rootScope) {
-    $rootScope.dummy = "";
+  $rootScope.dummy = "";
 });
+
 function ModalController($scope) {
-    $scope.dummy = "myDummy";
-};
+  $scope.dummy = "myDummy";
+}
